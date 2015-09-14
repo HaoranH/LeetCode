5dcb8b26177c5cd9f01d07b42c998a65df3a5289
hh6wh <leagark@qq.com>
2015-09-13 18:43:26 -0700

    add comments and explaination

--- src/aelongestPalindrome/Solution.java
+++ src/aelongestPalindrome/Solution.java
@@ -1,18 +1,21 @@
 package aelongestPalindrome;
-/*
- This works but it will exceed time limit on leetcode, even though its time complexity is O(n^2). 
- Gotta figure out a way to improve it.
- */
 
 public class Solution{
     public String longestPalindrome(String S){
         int length = S.length();
+        /*
+         According to the question description, there will only one unique palindrome for each input.
+         So when the length is 2, the string should have two same characters hence the palindrome.
+         */
         if(length == 0 || length == 1 || length == 2){
             return S;
         }
         String retStr = "";
         for(int n = 0; n<length; n++){
         	String temp = "";
+        	// I tried to figure out a way to avoid calling ckeckPalindrome twice for different situations
+        	// but it seemed that any additional conditional statement would cause extra increase of complexity
+        	// such as boundary conditions.
         	temp = checkPalindrome(S,n-1,n+1);
         	if(temp.length()>retStr.length()){
                 retStr = temp;
@@ -30,11 +33,9 @@ public class Solution{
         	left --;
         	right ++;
         }
-        return S.substring(left+1, right);
+        // When returning, the value of current variable 'left' and 'right' no longer fit for the 
+        // condition, so we have to return their previous value.
+        return S.substring(left+1, right); // 'substring' excludes right value.
     }
     
-    static public void main(String[] args){
-    	Solution test = new Solution();
-    	System.out.println(test.longestPalindrome("ccc"));
-    }
 }
