cea4f912f22335e657d56b35f5c596634f8d3226
HaoranH <lawrenceh91@gmail.com>
2015-09-17 13:30:16 -0700

    add solution to atoi

--- src/aclongestSubstringWithoutRepeatingCharacters/test.java
+++
@@ -1,7 +0,0 @@
-package aclongestSubstringWithoutRepeatingCharacters;
-
-public class test {
-	static public void main(String[] args){
-		System.out.println(123/10);
-	}
-}
---
+++ src/agmyAtoi/Solution.java
@@ -0,0 +1,44 @@
+package agmyAtoi;
+
+public class Solution {
+	public int myAtoi(String str){
+		/*
+		 *  So for this problem in leetcode, the specification, which should vary if found else where,
+		 *  is:
+		 *  0. Only numeric character should be converted into digits of the integer.
+		 *  1. Whitespace will be ignored only when it's located immediately BEFORE a numeric character. 
+		 *  2. '+' or '-' only makes sense when located immediately BEFORE a numeric character.
+		 *  3. If not the above cases, any non-numeric character encountered would result in 
+		 *  	returning the integer already converted so far, unless 4.
+		 *  4. If the current converted integer is larger than 2147483647 or less than -2147483648 the INT_MAX 
+		 *  or INT_MIN should be returned.
+		 *  
+		 *  
+		 *  Above specification only works for this question under current condition only. 
+		 * */
+		str = str.trim();
+        int length = str.length();
+        if(length == 0){
+            return 0;
+        }
+		int i = 0;
+		char firChar = str.charAt(0);
+		int flag = 1;
+		if (firChar == '-'){
+			flag = -1;
+			i++;
+		}else if(firChar == '+'){
+			i++;
+		}
+		long retVal = 0;
+		for (int n = i; n < length; n++){
+			if(Character.isDigit(str.charAt(n))){
+				retVal = retVal*10 + Character.getNumericValue(str.charAt(n));
+				if (retVal > Integer.MAX_VALUE){
+					return flag == 1?Integer.MAX_VALUE:Integer.MIN_VALUE;
+				}
+			}else break;
+		}
+		return (int)retVal*flag;
+    }
+}
