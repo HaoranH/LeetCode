23c9c008e6076de81ec75da4de7eb7ab827376f7
HaoranH <lawrenceh91@gmail.com>
2015-09-18 12:26:56 -0700

    add an update on the string solution

--- src/ahisPalindrome/strSolution.java
+++ src/ahisPalindrome/strSolution.java
@@ -2,7 +2,11 @@ package ahisPalindrome;
 
 /*
  *  IMO this is a better solution, in that it doesn't need consider negative number.
- *  Convert the integer to string and compare string at both ends. Voilà.
+ *  Convert the integer to string and compare string at both ends.
+ *  Update:
+ *  	So, even though this solution was accepted in Leetcode, it doesn't meet the requirement
+ *  	of 'Do this without extra space' in a way that restoring the string variable require space.
+ *   	FYI.
  * */
 public class strSolution {
 	public boolean isPalindrome(int x){
