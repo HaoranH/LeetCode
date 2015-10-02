b9b1ea136d5ef83e784095c17564b5d7b047931c
HaoranH <lawrenceh91@gmail.com>
2015-10-02 00:58:23 -0700

    clean up messy python code for isMatch backtrack solution

--- src/aiisMatch/Solution.py
+++ src/aiisMatch/Solution.py
@@ -2,17 +2,14 @@ class Solution:
     # @return a boolean
     # This works, but time complexity is high
     # This solution takes the advantage of python string slicing.
-    def isMatch(self, s, p):
-        if not p: return not s
-        if p[-1] == '*':
-            if self.isMatch(s,p[:-2]):
-                return True
-            if s and (s[-1] == p[-2] or p[-2] == '.') and self.isMatch(s[:-1],p):
-                return True
-        elif s and (s[-1] == p[-1] or p[-1] == '.') and self.isMatch(s[:-1],p[:-1]):
-            return True
-        return False
-    
-test = Solution()
-
-test.isMatch("aa","a*")
+   def isMatch(self,s,p):
+      if len(p) == 0:
+          return len(s) == 0
+      if len(p) == 1 or p[-1] != '*':
+          if s and (p[-1] == s[-1] or p[-1] == '.'):
+              return self.isMatch(s[:-1],p[:-1])
+      else:
+          if self.isMatch(s,p[:-2]): return True
+          if s and (s[-1]==p[-2] or p[-2]=='.'):
+              return self.isMatch(s[:-1],p)
+      return False
