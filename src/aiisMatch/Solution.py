f2e507cf59af7c267eae6e4a9705d3d7c9aa31f3
HaoranH <lawrenceh91@gmail.com>
2015-10-02 23:14:22 -0700

    add a quicker solution of python

--- src/aiisMatch/Solution.py
+++ src/aiisMatch/Solution.py
@@ -1,15 +1,23 @@
 class Solution:
     # @return a boolean
-    # This works, but time complexity is high
-    # This solution takes the advantage of python string slicing.
-   def isMatch(self,s,p):
-      if len(p) == 0:
-          return len(s) == 0
-      if len(p) == 1 or p[-1] != '*':
-          if s and (p[-1] == s[-1] or p[-1] == '.'):
-              return self.isMatch(s[:-1],p[:-1])
-      else:
-          if self.isMatch(s,p[:-2]): return True
-          if s and (s[-1]==p[-2] or p[-2]=='.'):
-              return self.isMatch(s[:-1],p)
-      return False
+    
+    # Faster method. Every ref[i][j] stands for the result of the ith substring in p and jth in s matching.
+    def isMatch(self,s,p):
+        ref = [[False]*(len(s)+1) for _ in xrange(len(p)+1)]
+        # Base case, where s and p match exactly.
+        ref[0][0] = True
+        # Cases where substring of p matches s and the last part is '.*' or 'A*'
+        for i in xrange(2,len(p)+1):
+            ref[i][0] = ref[i-2][0] and p[i-1] == '*'
+                        
+        for i in xrange(1,len(p)+1):
+            for j in xrange(1,len(s)+1):
+                if p[i-1] != '*':
+                    ref[i][j] = (s[j-1] == p[i-1] or p[i-1] == '.') and ref[i-1][j-1]
+                else:
+                    # Where the letter before * in p either matches last letter of s or not.
+                    ref[i][j] = ref[i-1][j] or ref[i-2][j]
+                    # Find the max number of repeats of *.
+                    if (s[j-1] == p[i-2] or p[i-2] == '.') and ref[i][j-1]:
+                        ref[i][j] = True
+        return ref[-1][-1]
